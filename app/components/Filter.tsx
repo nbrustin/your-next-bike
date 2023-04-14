15d5bdf482b3cc6d6c0b8c1039aacd697d510e5d
Nick Brustin <nbrustin@absnetwork.com>
2023-04-14 03:13:07 -0700

    adding bike type filter

--- app/components/Filter.tsx
+++ app/components/Filter.tsx
@@ -1,10 +1,26 @@
-import bicycleTypes from "";
+"use client";
+
+import { bicycleTypes } from "../../data/bicycleTypes";
+
+interface BicycleTypes {
+  value: number;
+  label: string;
+}
+
+const handleBikeType = (type: string) => {
+  console.log(type);
+  //need to adjust filter to use whatever type we're passing in...
+};
 
 export default function Filter() {
   return (
     <div>
       <h1 className="text-white text-5xl font-bold mb-2">Filter</h1>
-      <select></select>
+      <select onChange={(e) => handleBikeType(e.target.value)}>
+        {bicycleTypes.map((bicycleType: BicycleTypes) => (
+          <option key={bicycleType.value}>{bicycleType.label}</option>
+        ))}
+      </select>
     </div>
   );
 }
